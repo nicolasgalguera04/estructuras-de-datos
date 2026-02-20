0ca42dd043c35478a5691142828798f837ab7b7a
nicolasgalguera04 <nicolasgalguera04@gmail.com>
2026-02-20 16:16:01 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import lineal.MyArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyArrayList<Integer> L1 = new MyArrayList<>();
+        System.out.println(L1);
+        L1.add(3);
+        System.out.println(L1);
+        L1.add(4);
+        System.out.println(L1);
+        System.out.println(L1.get(1));
+
+    }
+    
+}
---
+++ src/lineal/MyArrayList.java
@@ -0,0 +1,320 @@
+package lineal;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+
+
+public class MyArrayList<E> implements List<E>{
+
+    private E[] data;
+    int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    @SuppressWarnings("unchecked")
+    public MyArrayList() {
+        data = (E[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    public MyArrayList(int initialCapacity) {
+        data = (E[]) new Object[initialCapacity];
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    private void ensureCapacity() {
+        if (size >= data.length) {
+            // duplicamos la capacidad
+            int newCapacity = data.length * 2;
+            E[] newData = (E[]) new Object[newCapacity];
+        
+            // copiamos los elementos
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+
+            data = newData; // apuntamos al nuevo array
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean add(E e) {
+        ensureCapacity();
+        data[size++] = e;
+        return true;
+    }
+
+    @Override
+    public void add(int index, E e) {
+        if (index < 0 || index > size())
+            throw new IndexOutOfBoundsException();
+        
+        ensureCapacity();
+        for (int i = size(); i > index; i--){
+            data[i] = data[i-1];
+        }
+        data[index] = e;
+    }
+
+    
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size(); i++) {
+            if (o.equals(data[i]) ) {
+                for (int j = i; j < size()-1; j++) {
+                    data[j] = data[j+1];
+                }
+                data[--size] = null;
+                return true;            
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size(); i++) {
+            if (o.equals(data[i]))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public E[] toArray() {
+        E[] newArray;
+
+        newArray = data.clone();
+
+        return newArray;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T[] toArray(T[] a) {
+
+        
+        if (a.length >= size()){
+            for (int i = 0; i < size(); i++) 
+                a[i] = (T) data[i];
+            return a;
+        } else {
+            T[] newArray = Arrays.copyOf(a, size());
+            for (int i = 0; i < size(); i++) 
+                newArray[i] = (T) data[i];
+            return newArray;
+        }
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        
+        for (Object o : c) {
+            if(!contains(o))
+                return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e : c) {
+            add(e);
+            
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        int offset = 0;
+        for (E e : c) {
+            add(index + offset, e);
+            offset++;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public E get(int index) {
+        return data[index];
+    }
+
+    @Override
+    public String toString() {
+        if (this.isEmpty())
+            return "[]";
+        String str = "[";
+        for (int i = 0; i < size()-1; i++) {
+            str += data[i] + ", ";
+        }
+
+        return str + data[size-1] + "]";
+
+    }
+
+    @Override
+    public E set(int index, E element) {
+        if (index < 0 || index >= size())
+            throw new IndexOutOfBoundsException();
+       
+        E previous = data[index];
+        data[index] = element;
+        return previous;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        
+        if (o == null) {
+            for (int i = 0; i < size(); i++){
+                if (get(i) == null)
+                    return i;
+            }
+        } else {
+            for (int i = 0; i < size(); i++){
+                if (o.equals(get(i)))
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = size() - 1; i >= 0 ; i--){
+                if (get(i) == null)
+                    return i;
+            }
+        } else {
+            for (int i = size() - 1; i >= 0 ; i--){
+                if (o.equals(get(i)))
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        MyListIterator<E> iterator = new MyListIterator<>();
+        return iterator;
+    }
+
+    class MyListIterator<E> implements ListIterator<E> {
+
+        int currentPosition;
+        boolean previousMade;
+        boolean nextMade;
+
+        public MyListIterator() {
+            currentPosition = 0;
+            previousMade = false;
+            nextMade = false;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentPosition < size();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public E next() {
+            if (!hasNext()){
+                throw new NoSuchElementException();
+            }
+            previousMade = false;
+            nextMade = true;
+            return (E) data[currentPosition++];
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return currentPosition > 0;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public E previous() {
+            if (!hasPrevious()){
+                throw new NoSuchElementException();
+            }
+            previousMade = true;
+            nextMade = false;
+            return (E) data[--currentPosition];
+        }
+
+        @Override
+        public int nextIndex() {
+            if (currentPosition == size()){
+                return size();
+            }
+            return currentPosition;
+        }
+
+        @Override
+        public int previousIndex() {
+            if (currentPosition == 0){
+                return 0;
+            }
+            return currentPosition - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (previousMade) {
+                // TODO
+                
+                previousMade = false;
+            } else if(nextMade){
+                // TODO
+                nextMade = false;
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+
+        @Override
+        public void set(E e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'set'");
+        }
+
+        @Override
+        public void add(E e) {
+            if (previousMade) {
+                // TODO
+                
+                previousMade = false;
+            } else if(nextMade){
+                // TODO
+                nextMade = false;
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+
+    }
+
+}
